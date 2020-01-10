7259f1511c1b1b27e8dc4f7a41bf849bef38e2bc
Nikolay <nanov12@abv.bg>
2020-01-10 14:07:42 +0200

    Changed Program.cs

--- OnlineExamer.Web/Program.cs
+++ OnlineExamer.Web/Program.cs
@@ -1,11 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
 namespace OnlineExamer.Web
 {
-    using Microsoft.AspNetCore.Hosting;
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.Hosting;
-    using System.IO;
-    using System.Reflection;
-
     public class Program
     {
         public static void Main(string[] args)
@@ -14,38 +19,11 @@ namespace OnlineExamer.Web
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-       Host.CreateDefaultBuilder(args)
-           .ConfigureWebHostDefaults(webBuilder =>
-           {
-               webBuilder.UseContentRoot(Directory.GetCurrentDirectory());
-               webBuilder.UseIISIntegration();
-               webBuilder.ConfigureAppConfiguration((hostingContext, config) =>
-               {
-                   var env = hostingContext.HostingEnvironment;
-
-                   config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                       .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
-                       .AddJsonFile($"appsettings.Local.json", optional: true, reloadOnChange: true);
-
-                   if (env.IsDevelopment())
-                   {
-                       var appAssembly = Assembly.Load(new AssemblyName(env.ApplicationName));
-                       if (appAssembly != null)
-                       {
-                           config.AddUserSecrets(appAssembly, optional: true);
-                       }
-                   }
-
-                   config.AddEnvironmentVariables();
-
-                   if (args != null)
-                   {
-                       config.AddCommandLine(args);
-                   }
-               });
-
-               webBuilder.UseStartup<Startup>();
-               webBuilder.UseSetting(WebHostDefaults.DetailedErrorsKey, "true");
-           });
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseSetting(WebHostDefaults.DetailedErrorsKey, "true");
+                });
     }
 }
